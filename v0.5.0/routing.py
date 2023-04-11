2560815a2ed4ea5275c540d39b7596abce273e2b
Noah Da Silva <noah@noahdasilva.com>
2023-04-10 22:47:34 -0400

    Updated project description

--- v0.5.0/routing.py
+++ v0.5.0/routing.py
@@ -1,11 +1,7 @@
 """ PROJECT : Satellite Network Simulation
 
     FILENAME : routing.py
-
-    DESCRIPTION :
-        Simulate a network of satellite nodes to compare performance 
-        compared to regular ground nodes.
-
+    
     FUNCTIONS :
         PacketRouting.PacketRouting()
         PacketRouting.edges_between_nodes()
@@ -13,23 +9,7 @@
         PacketRouting.draw()
         PacketRouting.dijskra_algorithm()
 
-    NOTES :
-        - ...
-
     AUTHOR(S) : Noah Da Silva    START DATE : 2022.11.26 (YYYY.MM.DD)
-
-    CHANGES :
-        - ...
-
-    VERSION     DATE        WHO             DETAILS
-    0.0.1a      2022.11.26  Noah            Creation of project.
-    0.0.2a      2023.01.09  Noah            Basic simulation of LEO satellite constellation.
-    0.0.2b      2023.01.19  Noah            Advanced simulation of LEO satellite constellation.
-    0.0.2c      2023.01.21  Noah/Ranul      Added distortion to LEO satellite orbit to better represent Mercator Projection.
-    0.1.0       2023.01.22  Noah            Added path from ground station to nearest satellite and shortest path algorithm.
-    0.1.1       2023.01.22  Noah            Allows to run multiple endpoint pairs at once (not recommended).
-    0.2.0       2023.03.17  Noah            Added MEO satellite constellation into routing calculations.
-    0.3.0       2023.03.22  Noah            Added load-balancing in form of a dynamic heatmap.
 """
 
 from math import dist
--- v0.5.0/simulation.py
+++ v0.5.0/simulation.py
@@ -2,10 +2,6 @@
 
     FILENAME : simulation.py
 
-    DESCRIPTION :
-        Simulate a network of satellite nodes to compare performance 
-        compared to regular ground nodes.
-
     FUNCTIONS :
         LEOSatellite.__init__()
         LEOSatellite.draw()
@@ -26,23 +22,7 @@
         Congestion.generate_congestion_heatmap()
         Congestion.refresh_congestion_heatmap()
 
-    NOTES :
-        - ...
-
     AUTHOR(S) : Noah Da Silva    START DATE : 2022.11.26 (YYYY.MM.DD)
-
-    CHANGES :
-        - ...
-
-    VERSION     DATE        WHO             DETAILS
-    0.0.1a      2022.11.26  Noah            Creation of project.
-    0.0.2a      2023.01.09  Noah            Basic simulation of LEO satellite constellation.
-    0.0.2b      2023.01.19  Noah            Advanced simulation of LEO satellite constellation.
-    0.0.2c      2023.01.21  Noah/Ranul      Added distortion to LEO satellite orbit to better represent Mercator Projection.
-    0.1.0       2023.01.22  Noah            Added path from ground station to nearest satellite and shortest path algorithm.
-    0.1.1       2023.01.22  Noah            Allows to run multiple endpoint pairs at once (not recommended).
-    0.2.0       2023.03.17  Noah            Added MEO satellite constellation into routing calculations.
-    0.3.0       2023.03.22  Noah            Added load-balancing in form of a dynamic heatmap.
 """
 
 from math import sin, pi, radians
