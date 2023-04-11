ed14af87f410588a53037e16f87f508ae5a073fb
Noah Da Silva <noah@noahdasilva.com>
2023-04-10 22:49:05 -0400

    Updated project description

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
     FILENAME : settings.py
 
     DESCRIPTION :
-        This module simulates a simple satellite network using Python and pygame. 
+        This program simulates a simple satellite network using Python and pygame. 
         It allows users to create and configure a network of LEO and MEO satellites 
         and ground stations, simulate packet routing, and visualize the network state 
         over time.
